15ad9768d83334d3f8800a507893fe4a6ee2da55
dreamlumos <30956230+dreamlumos@users.noreply.github.com>
2019-12-07 23:36:51 +0100

    Item

--- Item.java
+++ Item.java
@@ -25,6 +25,14 @@ public abstract class Item{
 		return y;
 	}
 
+	public void setX(int x){
+		this.x = x;
+	}
+
+	public void setY(int x){
+		this.y = y;
+	}
+
 	public String getNom(){
 		return nom;
 	}
