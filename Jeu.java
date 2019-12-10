a53bdf5fe5af0a4e2bf34eb6698e02b1cab67757
dreamlumos <30956230+dreamlumos@users.noreply.github.com>
2019-12-10 09:58:59 +0100

    Jeu

--- Jeu.java
+++ Jeu.java
@@ -2,6 +2,27 @@ public class Jeu{
 	
 	public static void main(String[] args){
 
-	
+	/* Q9 : */
+
+	Creature c1 = new Creature();
+
+	c1.courir();
+	c1.manger();
+	c1.courir();
+
+	/* Q13 : */
+
+	Creature c2 = new Creature();
+	Avatar a1 = new Avatar("Ava1", 45);
+
+	Monde monde = new Monde(5);
+
+	monde.ajouterItem(c1);
+	monde.ajouterItem(c2);
+	monde.ajouterItem(a1);
+
+	monde.afficher();
+
 	}
+
 }
