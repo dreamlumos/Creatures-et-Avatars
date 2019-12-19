61a394f68ff000faab53a6f0787bee871d9cbc6c
dreamlumos <30956230+dreamlumos@users.noreply.github.com>
2019-12-19 02:07:06 +0100

    Graphics, deplacement auto, sieste & energie

--- Creature.java
+++ Creature.java
@@ -1,22 +1,40 @@
+import java.util.ArrayList;
 import java.awt.*;
 import javax.swing.*;
-public class Creature extends Personnage {
+import javax.imageio.ImageIO;
+import java.io.*;
+
+public class Creature extends Personnage implements Sieste {
+/* 	Une créature commence avec un sac vide et 10 unités d'énergie. 
+	Elle dépense son énergie en se déplaçant et en regagne en prenant une sieste.
+	Plus elle a d'énergie, plus vite elle pourra courir.
+*/
 
 	private Sac leSac;
+	private int energie; 
+	private Monde monde;
 
-	public Creature() {
-		super(Noms.getNom());
+	public Creature(double poids, Monde monde) { //nom et position aléatoire
+		
+		super(Noms.getNom(), poids);
+
 		leSac = new Sac();
+		energie = 10;
+
+		this.monde = monde;
+		monde.ajouterItem((Item) this);
+
 	}
 
-	public Creature(String nom) {
+	public Creature(String nom, Monde monde) { //poids et position aléatoires
 		super(nom);
 		leSac = new Sac();
+		this.monde = monde;
+		monde.ajouterItem((Item) this);
 	}
 
-	public Creature(double poids) {
-		super(Noms.getNom(), poids);
-		leSac = new Sac();
+	public Creature(Monde monde) { //nom, poids et position aléatoires
+		this(Noms.getNom(), monde);
 	}
 
 	public void addAcc(Acc acc){
@@ -30,7 +48,7 @@ public class Creature extends Personnage {
 
 	public double getVitesse(){
 	/* Retourne la vitesse à laquelle peut courir la créature. */
-		double vitesse = getPoids()/4 - leSac.getPoids();
+		double vitesse = getPoids()/4 - leSac.getPoids() + energie/2;
 		if (vitesse < 0) {
 			return 0.0;
 		}
@@ -59,13 +77,86 @@ public class Creature extends Personnage {
 		System.out.println(super.toString()+" court a vitesse "+String.format("%.2f", getVitesse())+" km/h avec "+leSac.toString());
 	}
 
+	public void deplacementAuto(){
+	/* La créature bouge automatiquement d'une case dans une direction aléatoire. Le monde est torique. */
+
+		if (energie > 0) {
+
+			energie--;
+			
+			int oldX = getX();
+			int oldY = getY();
+			int mmax = 1;
+			int mmin = -1;		
+			int newX = super.getX() + (int)(Math.random() * ((mmax - mmin) + 1)) + mmin;
+			int newY = super.getY() + (int)(Math.random() * ((mmax - mmin) + 1)) + mmin;
+
+			if(newX > monde.getTaille() - 1)
+				newX = 0;
+			if(newX < 0)
+				newX = monde.getTaille() -1;
+			if(newY > monde.getTaille() - 1)
+				newY = 0;
+			if(newY < 0)
+				newY = monde.getTaille() -1;
+			
+			setX(newX);
+			setY(newY);
+			//System.out.println("Deplacement de "+getNom()+" de ("+oldX+","+oldY+") vers ("+newX+","+newY+") \n");
+
+		}
+
+	}
+
+	public void faireLaSieste(){
+	/* La créature prend une sieste si elle est sous un arbre. Elle regagne 2 unités d'énergie. */
+		ArrayList<Item> listeVoisins = monde.getVoisins(this);
+		for (Item i : listeVoisins){
+			if (i instanceof Arbre && i.getX() == getX() && i.getY() == getY()){
+				energie += 2;
+			}
+		}
+	}
+
 	public void dessiner(Graphics g, Monde m) {
 
 		int tc = m.getTailleCase();
+		File creature;
+		Image image;
+		String fileName;
+		String prenom = getNom();
+		prenom = prenom.substring(0, prenom.length()-1);
 
-		g.setColor(new Color(191, 154, 34)); //Couleur courante devient bleu
+		int i = 0;
+		while (i < Noms.getTabLength() && !prenom.equals(Noms.getTabNoms(i))){
+			i++;
+		}
 
-		g.fillRect(getX()*tc, getY()*tc, tc, tc); //Carré plein
+		switch (i){
+			case 0: 
+				fileName = "./images/bluemonster.png";
+				break;
+			case 1:
+				fileName = "./images/greenmonster.png";
+				break;
+			case 2:
+				fileName = "./images/pinkmonster.png";
+				break;
+			case 3:
+				fileName = "./images/purplemonster.png";
+				break;
+			default:
+				fileName = "./images/orangemonster.png";
+		}
+
+
+		try {
+			creature = new File(fileName);
+			image = ImageIO.read(creature);
+			g.drawImage(image, getX()*tc, getY()*tc, tc, tc, m); 
+		} catch (IOException e){
+			System.out.println(e.getMessage());
+		}
 
 	}
 }
